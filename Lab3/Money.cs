497f41d32ec4a3f04db0ee10789032d9ce7bf0b0
Przemysław Kudełka <235336@student.pwr.edu.pl>
2021-03-26 11:43:10 +0100

    Add project files.

---
+++ Lab3/Money.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    class Rates
+    {
+        public float EUR { set; get; }
+        public float GPB { set; get; }
+        public float PLN { set; get; }
+
+    }
+    class Money
+    {
+        public string disclaimer { set; get; }
+        public string license { set; get; }
+        public int timestamp { set; get; }
+        public string Base {set; get;}
+        public Rates Rates { set; get; }
+
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            load("https://openexchangerates.org/api/latest.json?app_id=72a8ed46e61f49c09af2017a0c3dd6a5&symbols=PLN,EUR,GBP");
+            Console.Read();
+        }// w ciagu godziny mamy zrobic uzycie api
+
+        public static async void load(string x)
+        {
+            string call = x;
+            HttpClient client = new HttpClient();
+            string json = await client.GetStringAsync(call);
+            Console.WriteLine(json);
+            Money m = JsonConvert.DeserializeObject<Money>(json);
+            Console.WriteLine(m.Rates.EUR);
+            Console.WriteLine(m.Rates.PLN);
+            Console.WriteLine(m.license);
+            Console.WriteLine(m.Base);
+            Console.WriteLine(m.timestamp); //sprawdzic jak jest opisany czas dokładnie
+            //mozemy dac ze uzytkownik wyklikuje, albo np. automat zeby sciagal dla wybranej waluty z okresu - ostatni rok
+
+    }
+    }
+}
